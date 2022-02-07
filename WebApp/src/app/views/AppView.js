9bdae502b3eefd68a41b8efd1c34ebc5551f8b17
Meindert van Duin <mvduin@ambrac.nl>
2022-02-07 14:09:09 +0100

    Create base RequireJs Backbone application

---
+++ WebApp/src/app/views/AppView.js
@@ -0,0 +1,65 @@
+﻿define([
+    '$',
+    'Backbone',
+    'Handlebars',
+], function ($, backbone, Handlebars) {
+    return backbone.View.extend({
+        el: '#app',
+        template: `
+            <div class="container">
+                <h1>Te doen lijst</h1>
+                
+                <div class="input-container">
+                    <input id="todo-input" type="text" placeholder="Vul een taak in">
+                    <button id="add-todo" type="submit">Voeg toe</button>
+                </div>
+                
+                <h4>Taakjes</h4>
+                {{#each .}}
+                <div class="todo-card">
+                    <span class="card-title">{{title}}</span>
+                    <button class="todo-close-button" index="{{@index}}">
+                    ✔️
+                    </button>
+                </div>
+                {{/each}}
+                
+            </div>
+        `,
+        compiledTemplate: null,
+
+        items: [ { title: 'asdasd' }],
+        
+        initialize: function () {
+            this.compiledTemplate = Handlebars.compile(this.template);   
+        },
+        
+        render: function () {
+            this.$el.html(this.compiledTemplate(this.items));
+        },
+        
+        events: {
+            'click #add-todo': 'onAddTodoClicked',
+            'click .todo-close-button': 'onTodoCloseClicked',
+        },
+
+        onAddTodoClicked: function (evt) {
+            evt.preventDefault();
+            
+            const todoTitle = $('#todo-input').val();
+            
+            this.items.push({
+                title: todoTitle,
+            });
+
+            this.$el.html(this.compiledTemplate(this.items));
+        },
+
+        onTodoCloseClicked: function (evt) {
+            const index = evt.target.getAttribute('index');
+            this.items.splice(index, 1);
+
+            this.$el.html(this.compiledTemplate(this.items));
+        },
+    })
+})
---
+++ WebApp/src/main.js
@@ -0,0 +1,32 @@
+﻿require.config( {
+    paths : {
+        // Dependencies
+        $           : '../vendor/jquery.min',
+        underscore  : '../vendor/underscore.min',
+        Backbone    : '../vendor/backbone.min',
+        Handlebars  : '../vendor/handlebars.min',
+        text        : '../vendor/text',
+        
+        // Application
+        AppView     : 'app/views/AppView',
+    },
+    shim: {
+        '$': {
+            exports: '$'
+        },
+        'Backbone': {
+            deps: ['underscore', '$'],
+            exports: 'Backbone'
+        },
+        'Handlebars': {
+            exports: 'Handlebars'
+        }
+    }
+});
+
+// initialization ...
+require(['$', 'AppView'], function($, AppView) {
+    $(function(){
+        new AppView().render();
+    });
+});
